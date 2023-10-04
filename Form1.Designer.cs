01ac93162edd09ca4d3d20c1335fb8d8d0d9640d
Thandolwethu316 <130293462+Thandolwethu316@users.noreply.github.com>
2023-10-04 12:10:57 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Guessing_Game2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnHint = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblHint = new System.Windows.Forms.Label();
+            this.txtguess = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(176, 288);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnHint
+            // 
+            this.btnHint.Location = new System.Drawing.Point(296, 288);
+            this.btnHint.Name = "btnHint";
+            this.btnHint.Size = new System.Drawing.Size(75, 23);
+            this.btnHint.TabIndex = 1;
+            this.btnHint.Text = "Hint";
+            this.btnHint.UseVisualStyleBackColor = true;
+            this.btnHint.Click += new System.EventHandler(this.btnHint_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(413, 288);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(549, 288);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblHint
+            // 
+            this.lblHint.AutoSize = true;
+            this.lblHint.Location = new System.Drawing.Point(375, 94);
+            this.lblHint.Name = "lblHint";
+            this.lblHint.Size = new System.Drawing.Size(26, 13);
+            this.lblHint.TabIndex = 4;
+            this.lblHint.Text = "Hint";
+            // 
+            // txtguess
+            // 
+            this.txtguess.Location = new System.Drawing.Point(339, 172);
+            this.txtguess.Name = "txtguess";
+            this.txtguess.Size = new System.Drawing.Size(100, 20);
+            this.txtguess.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtguess);
+            this.Controls.Add(this.lblHint);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnHint);
+            this.Controls.Add(this.btnGuess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnHint;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblHint;
+        private System.Windows.Forms.TextBox txtguess;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guessing_Game2
+{
+    public partial class Form1 : Form
+    {
+
+        private string[] footballClubs = { "Manchester United", "Real Madrid", "Barcelona", "Liverpool", "Bayern   " };
+        private string[] hints = { "English club with a red devil mascot", "Spanish club known for its Galácticos", "Famous Catalan club", "Premier League club with 'You'll Never Walk Alone'", "German club with multiple Bundesliga titles" };
+        private int currentWordIndex = 0;
+
+        private void DisplayHint()
+        {
+            if (currentWordIndex < footballClubs.Length)
+            {
+                lblHint.Text = "Hint: " + hints[currentWordIndex];
+            }
+            else
+            {
+                lblHint.Text = "Game Over!";
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayHint();
+
+
+        }
+
+        private void btnHint_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            if (currentWordIndex < footballClubs.Length)
+            {
+                string guess = txtguess.Text.ToLower();
+                string correctWord = footballClubs[currentWordIndex].ToLower();
+
+                if (guess == correctWord)
+                {
+                    MessageBox.Show("Correct guess!", "Guess Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    currentWordIndex++;
+                    txtguess.Clear();
+                    DisplayHint();
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect guess. Try again!", "Guess Result", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtguess.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure want to exit", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) { Application.Exit(); }
+        }
+    }
+}
